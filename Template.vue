d8cff83ceb4d65e0b5496fb39d99ec141566841b
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2021-11-21 20:58:50 +0100

    Update Template.vue

--- Template.vue
+++ Template.vue
@@ -1,1 +1,36 @@
+<template>
+  <h2>Component C</h2>
+  <ComponentE />
+</template>
 
+<script>
+import importedComponentName from './ComponentE'
+export default {
+  name: 'ComponentC',
+  components: {
+    importedComponentName,
+  },
+   props: {
+    modelValue: String,
+  },
+  data() {
+    return {
+      name: '',
+    }
+  },
+   methods: {
+    closePopup(name) {
+      this.showPopup = false
+      console.log('name', name)
+    },
+  },
+   provide() {
+    return {
+      userName: this.name,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
