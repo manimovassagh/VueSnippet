e0ed49e0cf440b6f571973ae15edb287f934adfc
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2021-11-22 22:39:24 +0100

    Add files via upload

---
+++ src/components/Fetcher.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.id }}<br/>{{ post.title }}<br/>{{post.body}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created() {
+    this.fetchDataFrom();
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    fetchDataFrom() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => (this.posts = response.data))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+ <h1> Hi Fucker</h1> 
+ <Fetcher/>
+</div>
+</template>
+
+<script>
+import Fetcher from "./Fetcher.vue"
+export default {
+  name: "HelloWorld",
+ components:{
+   Fetcher
+ }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
